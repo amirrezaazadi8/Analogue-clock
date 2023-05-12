98072c660df7d015230959fdaf56af144412a7b9
Amirreza Azadi <a.azadi2001@gmail.com>
2023-05-12 11:26:20 +0430

    drawTime and drawHand added to the app.js file

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ setInterval(() => {
 const drawClock = () => {
     drawFace(ctx, radius);
     drawNumbers(ctx, radius);
-    // drawTime(ctx, radius);
+    drawTime(ctx, radius);
 }
 
 const drawFace = (ctx, radius) => {
@@ -39,20 +39,48 @@ const drawFace = (ctx, radius) => {
 }
 
 
-const drawNumbers = (ctx,radius) => {
+const drawNumbers = (ctx, radius) => {
     let ang;
     let num;
     ctx.font = radius * 0.15 + "px arial";
     ctx.textBaseline = "middle";
     ctx.textAlign = "center";
-    for(num = 1; num < 13; num++){
-      ang = num * Math.PI / 6;
-      ctx.rotate(ang);
-      ctx.translate(0, -radius * 0.85);
-      ctx.rotate(-ang);
-      ctx.fillText(num.toString(), 0, 0);
-      ctx.rotate(ang);
-      ctx.translate(0, radius * 0.85);
-      ctx.rotate(-ang);
+    for (num = 1; num < 13; num++) {
+        ang = num * Math.PI / 6;
+        ctx.rotate(ang);
+        ctx.translate(0, -radius * 0.85);
+        ctx.rotate(-ang);
+        ctx.fillText(num.toString(), 0, 0);
+        ctx.rotate(ang);
+        ctx.translate(0, radius * 0.85);
+        ctx.rotate(-ang);
     }
+}
+
+const drawTime = (ctx, radius) => {
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+
+    hour = hour % 12;
+    hour = (hour * Math.PI / 6) + (minute * Math.PI / (6 * 60)) + (second * Math.PI / (360 * 60));
+    drawHand(ctx, hour, radius * 0.5, radius * 0.07);
+    //minute
+    minute = (minute * Math.PI / 30) + (second * Math.PI / (30 * 60));
+    drawHand(ctx, minute, radius * 0.8, radius * 0.07);
+    // second
+    second = (second * Math.PI / 30);
+    drawHand(ctx, second, radius * 0.9, radius * 0.02);
+}
+
+const drawHand = (ctx, pos, length, width) => {
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.lineCap = "round";
+    ctx.moveTo(0, 0);
+    ctx.rotate(pos);
+    ctx.lineTo(0, -length);
+    ctx.stroke();
+    ctx.rotate(-pos);
 }
