e3ddd6366efdba40da02f42209996ec97be145fb
Amirreza Azadi <a.azadi2001@gmail.com>
2023-05-12 10:56:33 +0430

    variables added to the project

---
+++ app.js
@@ -0,0 +1,12 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const radius = canvas.height / 2;
+
+ctx.translate(radius, radius);
+
+radius = radius * 0.9;
+
+const drawClock = () => {
+    
+}
