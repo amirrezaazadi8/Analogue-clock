4c6309e961255566afb559142ba66ed198d195a6
Amirreza Azadi <a.azadi2001@gmail.com>
2023-05-12 11:15:22 +0430

    drawNumbers function added to the app.js file

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ setInterval(() => {
 
 const drawClock = () => {
     drawFace(ctx, radius);
-    // drawNumbers(ctx, radius);
+    drawNumbers(ctx, radius);
     // drawTime(ctx, radius);
 }
 
@@ -36,4 +36,23 @@ const drawFace = (ctx, radius) => {
     ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
     ctx.fillStyle = '#333';
     ctx.fill();
+}
+
+
+const drawNumbers = (ctx,radius) => {
+    let ang;
+    let num;
+    ctx.font = radius * 0.15 + "px arial";
+    ctx.textBaseline = "middle";
+    ctx.textAlign = "center";
+    for(num = 1; num < 13; num++){
+      ang = num * Math.PI / 6;
+      ctx.rotate(ang);
+      ctx.translate(0, -radius * 0.85);
+      ctx.rotate(-ang);
+      ctx.fillText(num.toString(), 0, 0);
+      ctx.rotate(ang);
+      ctx.translate(0, radius * 0.85);
+      ctx.rotate(-ang);
+    }
 }
