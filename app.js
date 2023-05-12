c2de2b5f585894998c475f7150a24f6831623aae
Amirreza Azadi <a.azadi2001@gmail.com>
2023-05-12 11:01:35 +0430

    some changes added to the app.js

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-const radius = canvas.height / 2;
+const radius = (canvas.height / 2) * 0.9;
 
-ctx.translate(radius, radius);
-
-radius = radius * 0.9;
+ctx.translate(canvas.height / 2, canvas.width / 2);
 
 setInterval(() => {
     drawClock()
